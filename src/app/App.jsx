eee84bf72ca4e2de26b41f71c2aa1f6b1c7dd31d
Monika <monikababu18@gmail.com>
2025-11-21 11:23:12 +0530

    category

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "../home.jsx";
 import  Topbar from "../components/topbar/topbar.jsx"
 import  Footer from "../components/footer/footer.jsx"
+import Category from "../components/categories/category.jsx"
 import "./App.css";
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/topbar" element={<Topbar/>} />
         <Route path="/footer" element={<Footer/>} />
+        <Route path="/category" element={<Category />} />
+
       </Routes>
     </Router>
   );
